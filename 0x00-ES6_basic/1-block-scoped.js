df3ba273c6bc89cf958a313c6e2fc5783fd1212b
ThomasOselu <thomasoselu1@gmail.com>
2024-01-15 11:56:42 +0000

    The use of var function incase of hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,11 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-	  const task = true;
-	  
-	  const task2 = false;
+    // eslint-disable-next-line no-unused-vars, no-shadow
+    const task = true;
+    // eslint-disable-next-line no-unused-vars, no-shadow
+    const task2 = false;
   }
-	return [task, task2];
+
+  return [task, task2];
 }
