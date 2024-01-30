5a154595b6e9f91820375997dce68a693cc8bc6c
ThomasOselu <thomasoselu1@gmail.com>
2024-01-30 06:57:22 +0000

    javascript classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,11 @@
-import ClassRoom from './0-classroom.js';
+import Classroom from './0-classroom';
 
 function initializeRooms() {
-    const rooms = [
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(34)
-    ];
-    return rooms;
+  return [
+    new Classroom(19),
+    new Classroom(20),
+    new Classroom(34),
+  ];
 }
 
 export default initializeRooms;
